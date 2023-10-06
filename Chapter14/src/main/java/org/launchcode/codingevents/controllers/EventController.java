666eb0050aac29e997969a5ea911a152aff55869
slmetoxen <127230102+slmetoxen@users.noreply.github.com>
2023-10-06 11:33:53 -0500

    Add files via upload

---
+++ Chapter14/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,42 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.models.Event;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<Event>  events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping ("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(new Event(eventName));
+        return "redirect:/events";//route path ('/events') was needed for this redirect
+    }
+
+}
