c7d835c54b8d780fb3f1732cc529a667fd9a5f08
Sarah <slmetoxen@gmail.com>
2023-10-06 11:23:41 -0500

    Video Java Coding Events 2.1 Creating a Model Class-Chapter 14.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,12 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +18,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<Event>  events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping ("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(new Event(eventName));
+        return "redirect:/events";//route path ('/events') was needed for this redirect
     }
 
 }
